6526b3f2366078746bdf5a8ca1decfcc3e4261a4
AllannH <allann.hof@gmail.com>
2020-05-14 18:53:32 -0300

    Get

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-const frase = "a ligeira raposa marrom saltou sobre o cachorro cansado"
-const frase2 = "d oljhlud udsrvd pduurp vdowrx vreuh r fdfkruur fdqvdgr"
+const axios = require('axios')
+const sha1 = require('js-sha1')
+const fs = require('fs');
+const formData = require('form-data')
+const FormData = new formData()
 
-const token = "6d98bf236be36bea086ea142843275c7d41f6e16"
+const url = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=6d98bf236be36bea086ea142843275c7d41f6e16'
+const submit = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=6d98bf236be36bea086ea142843275c7d41f6e16'
 
+axios.get(url).then(resposta => {
+    var Data = resposta.data
+    CreateFile(Data)
+})
 
+setTimeout(SavePost, 4000)
 
-function Codificando(frase) {
+function SavePost() {
+    const Data = JSON.parse(fs.readFileSync('answer.json'))
+    const Result = Decodificando(Data.cifrado, Data.numero_casas)
+
+    Data.decifrado = Result
+    Data.resumo_criptografico = sha1(Result)
+
+    let obj = Object.assign({file: 'answer'}, Data)
+
+    fs.writeFile("answer.json", JSON.stringify(obj), (err) => {
+        if (err) throw err
+    })
+
+    setTimeout(() => {
+
+        
+    }, 4000)
+}
+
+function CreateFile(Data) {
+    fs.writeFile("answer.json", JSON.stringify(Data), (err) => {
+        if (err) throw err
+    })
+}
+
+function Codificando(frase, NumeroDeCasas, Original) {
     let ASCII = []
     let Codificado = []
     for (let i = 0; i < frase.length; i++) {
         let valor = frase.charCodeAt(i)
+        // 115 = 122 - numero de casas ; valor + numero de casas
+        if (valor >= 97 && valor <= (122 - NumeroDeCasas)) ASCII.push(valor + NumeroDeCasas)
+        // 116 = 123 - numero de casas ; -19 = 97 - (123- numero de casas)
+        else if (valor >= 123 - NumeroDeCasas && valor <= 122) ASCII.push(valor + (97 - (123 - NumeroDeCasas)))
 
-        if (valor >= 97 && valor <= 119) ASCII.push(valor + 3)
-        
-        else if (valor == 120 || valor == 121 || valor == 122) ASCII.push(valor - 23)
-        
-        else ASCII.push(valor) 
+        else ASCII.push(valor)
     }
 
     for (let i = 0; i < ASCII.length; i++) {
@@ -23,19 +57,19 @@ function Codificando(frase) {
         Codificado.push(letra)
     }
 
-    console.log(Codificado.join(''))
+    if (Codificado.join('') === Original) console.log("Codficação e Decodicação CONCLUIDA!")
 }
 
-function Decodficando(frase) {
+function Decodificando(Frase, NumeroDeCasas) {
     let ASCII = []
     let Decodificado = []
-    for (let i = 0; i < frase.length; i++) {
-        let valor = frase.charCodeAt(i)
+    for (let i = 0; i < Frase.length; i++) {
+        let valor = Frase.charCodeAt(i)
+        // 104 = 97 + 7 ; valor - numero de casas
+        if (valor >= (97 + NumeroDeCasas) && valor <= 122) ASCII.push(valor - NumeroDeCasas)
+        // 103 = 96 + 7 ; 19 = (123-NdC) - 97 
+        else if (valor >= 97 && valor <= (96 + NumeroDeCasas)) ASCII.push(valor + (123 - NumeroDeCasas) - 97)
 
-        if (valor >= 100 && valor <= 122) ASCII.push(valor - 3)
-
-        else if (valor == 97 || valor == 98 || valor == 99) ASCII.push(valor + 23)
-        
         else ASCII.push(valor)
     }
 
@@ -43,6 +77,6 @@ function Decodficando(frase) {
         var letra = String.fromCharCode(ASCII[i])
         Decodificado.push(letra)
     }
-
-    console.log(Decodificado.join(''))
+    // Codificando(Decodificado.join(''), NumeroDeCasas, Frase)
+    return Decodificado.join('');
 }
